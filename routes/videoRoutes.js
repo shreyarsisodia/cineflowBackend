509de07d8fc50932152a847d88ade9f4e5599110
shreya sisodia <shreyarsisodia@gmail.com>
2025-03-17 17:04:43 +0530

    made fixes

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -26,7 +26,7 @@ const s3 = new S3Client({
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: "cineflow-videofiles", //process.env.AWS_S3_BUCKET,
+    bucket: "cineflow-videos", //process.env.AWS_S3_BUCKET,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: (req, file, cb) => {
       cb(null, `videos/${Date.now()}-${path.basename(file.originalname)}`);
